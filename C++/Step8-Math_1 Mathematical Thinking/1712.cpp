10e4e7b1e42074285063bcf5cbffc344e5a6995f
YebinKim <yb2316@naver.com>
2019-09-03 23:06:50 +0900

    :zap: Solve Mathematical Thinking Algorithm Questions
    Solve algorithm questions using mathematical thinking at Baekjoon Online judge

---
+++ C++/Step8-Math_1 Mathematical Thinking/1712.cpp
@@ -0,0 +1,28 @@
+//
+//  1712.cpp
+//  Step8-Step8-Math_1 Mathematical Thinking
+//
+//  Created by 김예빈 on 2019. 9. 3..
+//  Copyright © 2019년 김예빈. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    int a, b, c, result;
+    
+    cin >> a >> b >> c;
+    
+    if(c - b > 0)
+        result = a / (c - b) + 1;
+    else
+        result = -1;
+    
+    cout << result;
+    
+    return 0;
+}
---
+++ C++/Step8-Math_1 Mathematical Thinking/2839.cpp
@@ -0,0 +1,39 @@
+//
+//  2839.cpp
+//  Step8-Step8-Math_1 Mathematical Thinking
+//
+//  Created by 김예빈 on 2019. 9. 3..
+//  Copyright © 2019년 김예빈. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    int n, maxFive, maxThree, result = 0;
+    
+    cin >> n;
+    
+    maxFive = n / 5;
+    
+    while(maxFive >= 0) {
+        maxThree = n - (maxFive * 5);
+        
+        if (maxThree % 3 == 0) {
+            result = maxThree / 3 + maxFive;
+            break;
+        }
+        else
+            maxFive--;
+    }
+    
+    if(result <= 0)
+        cout << -1;
+    else
+        cout << result;
+    
+    return 0;
+}
