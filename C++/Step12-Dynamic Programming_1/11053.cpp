9589e3995c1de42cbb005f77e6b12d4ada754009
YebinKim <yb2316@naver.com>
2019-09-11 23:37:17 +0900

    :zap: Solve Dynamic Programming Algorithm Questions
    Solve algorithm questions using dynamic programming at Baekjoon Online Judge

---
+++ C++/Step12-Dynamic Programming_1/11053.cpp
@@ -0,0 +1,46 @@
+//
+//  11053.cpp
+//  Step12-Dynamic Programming_1
+//
+//  Created by 김예빈 on 2019. 9. 11..
+//  Copyright © 2019년 김예빈. All rights reserved.
+//
+
+// Using LIS(Longest Increasing Subsequence)
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    int n, min, result = 0, i, j;
+    
+    cin >> n;
+    
+    int A[n + 1];
+    int dp[n + 1];
+    dp[0] = 0;
+    
+    for(i = 1; i <= n; i++)
+        cin >> A[i];
+    
+    for(i = 1; i <= n; i++) {
+        min = 0;
+        
+        for(j = 0; j < i; j++)
+            if(A[i] > A[j]) {
+                if (min < dp[j])
+                    min = dp[j];
+            }
+        
+        dp[i] = min + 1;
+        
+        if(result < dp[i])
+            result = dp[i];
+    }
+    
+    cout << result;
+    
+    return 0;
+}
