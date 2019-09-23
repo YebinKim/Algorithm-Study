e838ea06f739a5398a8ecbd622b6ab7ac8d9b0ac
YebinKim <yb2316@naver.com>
2019-09-23 23:25:23 +0900

    :zap: Solve Priority Queue Algorithm Questions
    Solve algorithm questions using priority queue at Baekjoon Online Judge

---
+++ C++/Step20-Priority Queue/11279.cpp
@@ -0,0 +1,36 @@
+//
+//  11279.cpp
+//  Step20-Priority Queue
+//
+//  Created by 김예빈 on 2019. 9. 23..
+//  Copyright © 2019년 김예빈. All rights reserved.
+//
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    
+    int n, x;
+    priority_queue<int> pq;
+    
+    cin >> n;
+    
+    while(n--) {
+        cin >> x;
+        
+        if(x == 0) {
+            if(pq.empty())
+                cout << "0\n";
+            else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        } else pq.push(x);
+    }
+
+    return 0;
+}
