6908d744eac20ece60bae76cc281be24ade2c22c
YebinKim <yb2316@naver.com>
2019-09-28 23:05:47 +0900

    :zap: Solve Binary Search Algorithm Questions
    Solve algorithm questions using binary search at Baekjoon Online Judge

---
+++ C++/Step19-Binary Search/1654.cpp
@@ -0,0 +1,40 @@
+//
+//  1654.cpp
+//  Step19-Binary Search
+//
+//  Created by 김예빈 on 2019. 9. 28..
+//  Copyright © 2019년 김예빈. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int k, n, result;
+    long long max = 0, mid, left = 0, right = pow(2, 31) - 1;
+    
+    cin >> k >> n;
+    
+    int arr[k];
+    
+    for(int i = 0; i < k; i++)
+        cin >> arr[i];
+    
+    while(left <= right) {
+        result = 0;
+        mid = (left + right) / 2;
+        
+        for(int j = 0; j < k; j++)
+            result = result + arr[j] / mid;
+        
+        if(result >= n) {
+            left = mid + 1;
+            if(mid > max) max = mid;
+        } else if(result < n) right = mid-1;
+    }
+    
+    cout << max;
+    
+    return 0;
+}
