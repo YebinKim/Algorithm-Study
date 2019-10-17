03300f649e3e8a9fe9eae1fa4323531be65b3db9
YebinKim <yb2316@naver.com>
2019-10-17 22:57:40 +0900

    :zap: Re-Solve Sorting Algorithm Problems
    Re-Solve algorithm problems using sorting at Baekjoon Online Judge

--- BaekJoon Online Judge/C++/Step11-Sorting/2750.cpp
+++ BaekJoon Online Judge/C++/Step11-Sorting/2750.cpp
@@ -2,17 +2,21 @@
 //  2750.cpp
 //  Step11-Sorting
 //
-//  Created by 김예빈 on 2019. 9. 5..
-//  Copyright © 2019년 김예빈. All rights reserved.
+//  Created by 김예빈 on 2019/10/17.
+//  Copyright © 2019 김예빈. All rights reserved.
 //
+
+#define SWAP(x, y, t) (t=x, x=y, y=t)
 
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-// Using Sorting Algorithms with Time Complexity O(n^2)
-void insertSort(vector<int> &);
+void bubbleSort(vector<int>&);
+void insertSort(vector<int>&);
 
+// Using Sorting Algorithms with Time Complexity O(n^2)
 int main(int argc, const char * argv[]) {
     cin.tie(NULL);
     ios::sync_with_stdio(false);
@@ -26,7 +30,8 @@ int main(int argc, const char * argv[]) {
     for(i = 0; i < n; i++)
         cin >> sort[i];
     
-    insertSort(sort);
+    bubbleSort(sort);
+//    insertSort(sort);
     
     for(i = 0; i < n; i++)
         cout << sort[i] << "\n";
@@ -34,8 +39,17 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
-void insertSort(vector<int> &v) {
-    int key, i, j;
+void bubbleSort(vector<int>& v) {
+    int size = v.size(), temp;
+    
+    for(int i = 0; i < size; i++)
+        for(int j = i + 1; j < size; j++)
+            if(v[i] > v[j])
+                SWAP(v[i], v[j], temp);
+}
+
+void insertSort(vector<int>& v) {
+    int i, j, key;
     
     for(i = 1; i < v.size(); i++) {
         key = v[i];
