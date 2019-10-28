5b53ad03932ad79a84f586944b98941f21840ba2
YebinKim <yb2316@naver.com>
2019-10-28 14:30:27 +0900

    :zap: Re-Solve I/O and Arithmetic Algorithm Problems
    Re-Solve algorithm problems using I/O and arithmetic at Baekjoon Online Judge

--- BaekJoon Online Judge/Swift/Step01-I:O and Arithmetic/2588.swift
+++ BaekJoon Online Judge/Swift/Step01-I:O and Arithmetic/2588.swift
@@ -2,7 +2,7 @@
 //  2588.swift
 //  Step01-I/O and Arithmetic
 //
-//  Created by Yebin Kim on 2019/10/27.
+//  Created by Yebin Kim on 2019/10/28.
 //  Copyright © 2019 김예빈. All rights reserved.
 //
 
@@ -11,36 +11,14 @@ import Foundation
 var fir: Int! = Int(readLine()!)
 var sec: Int! = Int(readLine()!)
 
-let ans = fir * sec
-let mul1 = fir * (sec % 10)
-let mul2 = fir * (sec / 10 % 10)
-let mul3 = fir * (sec / 100 % 10)
+if let fir = fir, let sec = sec {
+    let ans = fir * sec
+    let mul1 = fir * (sec % 10)
+    let mul2 = fir * (sec / 10 % 10)
+    let mul3 = fir * (sec / 100 % 10)
 
-print(mul1)
-print(mul2)
-print(mul3)
-print(ans)
-
-//var fir = Int(readLine()!)
-//var sec = Int(readLine()!)
-//
-//var ans: Int = 0, mul1: Int = 0, mul2: Int = 0, mul3: Int = 0
-//
-//if let fir = fir {
-//    ans = fir
-//    mul1 = fir
-//    mul2 = fir
-//    mul3 = fir
-//}
-//
-//if let sec = sec {
-//    ans *= sec
-//    mul1 *= (sec % 10)
-//    mul2 *= (sec / 10 % 10)
-//    mul3 *= (sec / 100 % 10)
-//
-//    print(mul1)
-//    print(mul2)
-//    print(mul3)
-//    print(ans)
-//}
+    print(mul1)
+    print(mul2)
+    print(mul3)
+    print(ans)
+}
