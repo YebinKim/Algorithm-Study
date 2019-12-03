87c9a8b48ac7c6dc4246b093bcc58f23305ead44
YebinKim <yb2316@naver.com>
2019-12-03 15:39:02 +0900

    :zap: Solve Backtracking Algorithm Problems
    Solve algorithm problems using backtracking at Baekjoon Online Judge

---
+++ BaekJoon Online Judge/Swift/Step13-Backtracking/15650.swift
@@ -0,0 +1,43 @@
+//
+//  15650.swift
+//  Step13-Backtracking
+//
+//  Created by Yebin Kim on 2019/12/3.
+//  Copyright © 2019 김예빈. All rights reserved.
+//
+
+import Foundation
+
+let input = readLine()!.split(separator: " ").map{ Int($0)! }
+let N = input[0]
+let M = input[1]
+
+var visit = Array<Bool>(repeating: false, count: 9)
+var graph = Array<Int>()
+
+func dfs(_ idx: Int, _ start: Int) {
+    if start == M {
+        for i in 0..<graph.count {
+            print(graph[i], terminator: " ")
+        }
+        
+        print()
+        return
+    }
+    
+    for i in idx..<N {
+        if visit[i] == true {
+            continue
+        }
+        
+        visit[i] = true
+        graph.append(i + 1)
+        
+        dfs(i, start + 1)
+        
+        visit[i] = false
+        graph.removeLast()
+    }
+}
+
+dfs(0, 0)
