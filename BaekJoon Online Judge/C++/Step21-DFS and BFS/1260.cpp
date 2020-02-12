540ece068551b999d225dce260b1a19ebfb541cf
YebinKim <yb2316@naver.com>
2020-02-12 23:12:46 +0900

    ⚡️ Re-Solve DFS and BFS Algorithm Problems
    Re-Solve algorithm problems using DFS and BFS at Baekjoon Online Judge

--- BaekJoon Online Judge/C++/Step21-DFS and BFS/1260.cpp
+++ BaekJoon Online Judge/C++/Step21-DFS and BFS/1260.cpp
@@ -2,45 +2,47 @@
 //  1260.cpp
 //  Step21-DFS and BFS
 //
-//  Created by 김예빈 on 2019. 9. 20..
-//  Copyright © 2019년 김예빈. All rights reserved.
+//  Created by Yebin Kim on 2020/02/12.
+//  Copyright © 2020 김예빈. All rights reserved.
 //
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
+#include <algorithm>
+#include <cstring>
 using namespace std;
 
 void dfs(int, vector<int>[], bool[]);
 void bfs(int, vector<int>[], bool[]);
 
-int main(int argc, const char * argv[]) {
+int main() {
     cin.tie(NULL);
     ios::sync_with_stdio(false);
     
-    int n, m, v, v1, v2;
-    
-    cin >> n >> m >> v;
+    int n, m, start;
+    cin >> n >> m >> start;
     
     vector<int> graph[n + 1];
     bool check[n + 1];
-    fill(check, check + n + 1, false);
+    memset(check, false, sizeof(check));
     
     for(int i = 0; i < m; i++) {
-        cin >> v1 >> v2;
-        graph[v1].push_back(v2);
-        graph[v2].push_back(v1);
+        int u, v;
+        cin >> u >> v;
+        
+        graph[u].push_back(v);
+        graph[v].push_back(u);
     }
     
     for(int i = 1; i <= n; i++)
         sort(graph[i].begin(), graph[i].end());
     
-    dfs(v, graph, check);
+    dfs(start, graph, check);
     cout << "\n";
     
-    fill(check, check + n + 1, false);
-    bfs(v, graph, check);
+    memset(check, false, sizeof(check));
+    bfs(start, graph, check);
     
     return 0;
 }
@@ -51,17 +53,16 @@ void dfs(int start, vector<int> graph[], bool check[]) {
     
     for(int i = 0; i < graph[start].size(); i++) {
         int next = graph[start][i];
-        if(check[next] == false) {
+        
+        if(check[next] == false)
             dfs(next, graph, check);
-        }
     }
 }
 
 void bfs(int start, vector<int> graph[], bool check[]) {
     queue<int> q;
-    
-    q.push(start);
     check[start] = true;
+    q.push(start);
     
     while(!q.empty()){
         int temp = q.front();
